003cb38b6fa33711c7dd51fa3d7b307d99fce9f6
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-04-16 17:51:47 +0600

    Extension : Product Extension Initialized

---
+++ custom_addons/product_extension_adeeb/.history/__manifest___20210416174831.py
@@ -0,0 +1,25 @@
+{
+    'name': "Product Extension -Adeeb",
+    'version': '1.0',
+    'summary': "First Custom Addon",
+    'sequence': 10,
+    'description': """Keeps Books' Details""",
+    'depends': ['base',
+                'contacts',
+                'sale'
+                ],
+    'data': [
+        'views/books_view.xml',
+        'views/authors_view.xml',
+        'views/languages_view.xml',
+        'views/shelves_view.xml',
+        'views/quotations_view.xml',
+        'views/product_brand_view.xml',
+        'views/dealer_name_view.xml',
+        'views/products_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
---
+++ custom_addons/product_extension_adeeb/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Product Extension - Adeeb",
+    'version': '1.0',
+    'summary': "Product Extension - Adeeb",
+    'sequence': 10,
+    'description': """Extends Products with Brand & Dealer""",
+    'depends': ['base',
+                'sale'
+                ],
+    'data': [
+        'views/product_brand_view.xml',
+        'views/dealer_name_view.xml',
+        'views/products_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
